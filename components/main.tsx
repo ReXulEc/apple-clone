c5e38b04a4e8469afdbf3eb7b7528facb3f86786
Mert Doğu <mertdoguonder@protonmail.com>
2022-06-10 21:45:27 +0300

    :lipstick: Centered ```MacBook``` img

--- components/main.tsx
+++ components/main.tsx
@@ -4,10 +4,10 @@ export const Main: FC = () => {
   return (
     <div className="text-[#c6e2e6] beyazbg">  
       <div className="flex justify-center items-center py-40 text-[#1d1d1f]">
-        <div className="flex w-10/12">
-          <div className="flex items-center w-1/2">
+        <div className="flex w-10/12 items-center justify-center">
+          <div className="flex items-center">
             <div className="grid space-y-4 justify-items-center ">
-              <p className="md:text-6xl text-4xl font-semibold tracking-tight">MacBook Air</p>
+              <p className="md:text-6xl text-xl font-semibold tracking-tight">MacBook Air</p>
                 <div className="flex space-x-4 items-center">
                   <p className=" font-light text-2xl">Supercharged by</p>
                   <img src="https://www.apple.com/v/home/takeover/c/images/overview/macbook-air/supercharged_mba__d3aczbb0h4ia_large.jpg" alt="" />
