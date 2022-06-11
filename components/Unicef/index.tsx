2c256af36603a45ead8dad511c995e69bf19909e
Mert Doğu <mertdoguonder@protonmail.com>
2022-06-11 22:30:47 +0300

    :lipstick: added ```Unicef``` section to the ```Main Page```

---
+++ components/Unicef/index.tsx
@@ -0,0 +1,8 @@
+export const Unicef = () => {
+  return (
+    <div className="flex items-center justify-center bg-[#f5f5f7] p-4">
+        <img className="" src="/assets/unicef.png" alt="" />
+        <p className="flex items-center text-sm ml-2 text-blue-600">Donate to support families affected by the war in Ukraine <svg className="w-3 h-3 ml-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7" /></svg></p>
+    </div>
+  );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import { Navbar } from "@components/Navbar";
+import { Unicef } from "@components/Unicef";
 import { Main } from "@components/main";
 import { Footer } from "@components/Footer"
 
@@ -6,6 +7,7 @@ const Home = () => {
   return (
       <div className="">
         <Navbar />
+        <Unicef />
         <Main />
         <Footer />
       </div>
